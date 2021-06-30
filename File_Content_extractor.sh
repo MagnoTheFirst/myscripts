9fe91b2fb5db27f753abcc046e7c11b3ab71c12a
Laneri Alejandro, INI-DOS-EAS <Alejandro.Laneri1@swisscom.com>
2021-06-30 11:51:39 +0200

    First Lady

---
+++ File_Content_extractor.sh
@@ -0,0 +1,6 @@
+File_Content_extractor.sh
+  #!/bin/bash
+  #Simple Shell script that extracts lines of a file and writes it into another file
+  sed -n -e 42p -e 44p -e 47p -e 49p -e 59p -e 64p -e 69p -e 74p -e 79p -e 84p <source_file> > <destination file>
+  
+
